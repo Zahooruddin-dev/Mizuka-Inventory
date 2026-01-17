bc88a229fc7ddf9aa4ad5ef703202bdde7fd6688
Zahooruddin-dev <mzkhan886@gmail.com>
2026-01-17 21:02:14 +0500

    <final>

--- frontend/src/services/productService.js
+++ frontend/src/services/productService.js
@@ -1,57 +1,60 @@
-import axios from "axios";
+import axios from 'axios';
 
 const productService = () => {
-    const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/products`;
-    const CLOUDINARY_URL = import.meta.env.VITE_CLOUDINARY_URL;
+	const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/products`;
+	const CLOUDINARY_URL = import.meta.env.VITE_CLOUDINARY_URL;
 
-    return {
-        getAll: async () => {
-            const response = await axios.get(API_BASE_URL);
-            return response.data;
-        },
+	return {
+		getAll: async () => {
+			const response = await axios.get(API_BASE_URL);
+			return response.data;
+		},
 
-        getProductCount: async () => {
-            const products = await axios.get(API_BASE_URL);
-            return products.data.length;
-        },
+		getProductCount: async () => {
+			const products = await axios.get(API_BASE_URL);
+			return products.data.length;
+		},
 
-        getStocksCount: async () => {
-            const response = await axios.get(API_BASE_URL);
-        
-            const totalStock = response.data.reduce(
-                (sum, product) => sum + Number(product.quantity),
-                0
-            );
-            return totalStock;
-        },
+		getStocksCount: async () => {
+			const response = await axios.get(API_BASE_URL);
 
-        uploadImage: async (file) => {
-            const formData = new FormData();
-            formData.append("file", file);
-            formData.append("upload_preset", "unsigned_upload");
-            
-            const response = await axios.post(CLOUDINARY_URL, formData);
-            return response.data.secure_url;
-        },
+			const totalStock = response.data.reduce(
+				(sum, product) => sum + Number(product.quantity),
+				0,
+			);
+			return totalStock;
+		},
 
-        create: async (productData) => {
-            const response = await axios.post(API_BASE_URL, productData);
-            console.log(response.data);
-            return response.data;
-        },
+		uploadImage: async (file) => {
+			const formData = new FormData();
+			formData.append('file', file);
+			formData.append('upload_preset', 'unsigned_upload');
 
-        delete: async (productId) => {
-            const category = await axios.get(`${API_BASE_URL}/${productId}`);
-            if (category.data) {
-                await axios.delete(`${API_BASE_URL}/${productId}`);
-            }
-        },
+			const response = await axios.post(CLOUDINARY_URL, formData);
+			return response.data.secure_url;
+		},
 
-        update: async (productId, updatedData) => {
-            const response = await axios.put(`${API_BASE_URL}/${productId}`, updatedData);
-            return response.data;
-        }
-    };
+		create: async (productData) => {
+			const response = await axios.post(API_BASE_URL, productData);
+			console.log(response.data);
+			return response.data;
+		},
+
+		delete: async (productId) => {
+			const category = await axios.get(`${API_BASE_URL}/${productId}`);
+			if (category.data) {
+				await axios.delete(`${API_BASE_URL}/${productId}`);
+			}
+		},
+
+		update: async (productId, updatedData) => {
+			const response = await axios.put(
+				`${API_BASE_URL}/${productId}`,
+				updatedData,
+			);
+			return response.data;
+		},
+	};
 };
 
 export default productService;
